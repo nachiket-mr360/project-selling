27acf1a7258d248409ade891bb753d261d1bcc17
nachiket <nachiket360chavan@gmail.com>
2026-04-14 12:38:10 +0530

    deploy build files

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"https://nachiket-mr360.github.io/project-selling/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
